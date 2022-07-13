53bdd41b3745e291c9053e0ec899d2c682bc306f
enajera1 <eriknajera013@gmail.com>
2022-07-13 09:26:18 -0500

    comment model completed

--- controllers/pizza-controller.js
+++ controllers/pizza-controller.js
@@ -3,7 +3,13 @@ const { Pizza } = require('../models');
 const pizzaController = {
   // get all pizzas
   getAllPizza(req, res) {
-    Pizza.find({}) // '.find is a mongoose method like 'findAll' is a sequelize. 
+    Pizza.find({})
+      .populate({
+        path: 'comments',
+        select: '-__v'
+      })
+      .select('-__v')
+      .sort({ _id: -1 }) //sort in descending order
       .then(dbPizzaData => res.json(dbPizzaData))
       .catch(err => {
         console.log(err);
@@ -14,8 +20,12 @@ const pizzaController = {
   // get one pizza by id
   getPizzaById({ params }, res) {
     Pizza.findOne({ _id: params.id })
+      .populate({
+        path: 'comments',
+        select: '-__v'
+      })
+      .select('-__v')
       .then(dbPizzaData => {
-        // If no pizza is found, send 404
         if (!dbPizzaData) {
           res.status(404).json({ message: 'No pizza found with this id!' });
           return;
